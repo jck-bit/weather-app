6066329fe0a830a2a62bfa66d802d401124c66d5
jck-bi <jckkinyanjui@gmail.com>
2021-07-05 05:28:28 +0300

    some js

---
+++ app.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = 'https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},{long}';
+
+            fetch(api)
+              .then(Response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            });
+
+        });
+
+     
+    }
+});
